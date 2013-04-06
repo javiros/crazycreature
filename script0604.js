f137ee148d845f94f28e6afd868fbcb0e20458b7
paul mannion <mannion.paul@gmail.com>
2013-04-06 15:30:56 +0100

    script.js and css file modified

---
+++ script0604.js
@@ -0,0 +1,70 @@
+
+$(document).on("ready", function(){
+  var questions = [
+	{"text": "What's the capital of Italy", "answers": ["Rome", "Paris", "Oslo", "Dublin"], "correct": "Rome" },
+	{"text": "What is the capital of Germany", "answers": ["Zagreb", "London", "Viena", "Berlin"], "correct": "Berlin" },
+	{"text": "What is the capital of Ireland", "answers": ["Belfast", "Dublin", "Chaco", "Madrid"], "correct": "Dublin" },
+	{"text": "What is the capital of Germany", "answers": ["Zagreb", "London", "Viena", "Berlin"], "correct": "Berlin" },
+
+  ];
+	var score = 0
+	var display_question = (function () {
+		var question = questions[score];
+		
+		$("#question").text(question.text);
+		$("#answer").empty();
+		for( var i in question.answers) {
+			var answer = question.answers[i]
+			$("#answer").append("<li>" + answer + "</li>");
+		}
+		
+		$("#answer li").on("click", validate_answer);
+	});
+	
+	var validate_answer = (function () {
+		if ($(this).text() == questions[score].correct) {
+			score++;
+			if (questions.length > score) {
+				display_question();
+				countdown = max_countdown;
+			} else {
+				$("#question").text("you are great quizz finished!");
+				$("#answer").empty();
+				clearInterval(countdown_interval);
+			}	
+		} else {
+		  display_error();
+		}
+				display_score();
+
+	});
+	
+	var display_error = (function() {
+		  $("#question").text("you suck");
+		  $("#answer").empty();
+		  clearInterval(countdown_interval);
+	});
+	
+	var display_score = (function (){ 
+			$("#score").text(score * 100);
+			
+	});
+	
+	var max_countdown = 1000000000000;
+	var countdown = max_countdown;
+	var display_countdown = (function() {
+		$("#countdown").text(countdown);
+	});
+	
+	var countdown_interval = setInterval(function() {
+			display_countdown();
+			countdown--;
+			if(countdown < 0){
+				display_error();
+			}
+			}, 1000);
+	
+	display_question();
+	display_score();
+
+});	
