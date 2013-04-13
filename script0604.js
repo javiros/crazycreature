3d924629dacac1211cfc36cd7dbd96b258e97bec
broonzy <ger_daly@yahoo.com>
2013-04-13 18:43:41 +0100

    stage 1 tidy up

--- script0604.js
+++ script0604.js
@@ -1,18 +1,17 @@
 
 $(document).on("ready", function(){
   var questions = [
-	{"text": "What's the capital of Italy", "answers": ["Rome", "Paris", "Oslo", "Dublin"], "correct": "Rome", "picture": "images/monkey_1.png" },
-	{"text": "What is the capital of Germany", "answers": ["Zagreb", "London", "Viena", "Berlin"], "correct": "Berlin" },
-	{"text": "What is the capital of Ireland", "answers": ["Belfast", "Dublin", "Chaco", "Madrid"], "correct": "Dublin" },
-	{"text": "What is the capital of Germany", "answers": ["Zagreb", "London", "Viena", "Berlin"], "correct": "Berlin" },
-
+	{"text": "What's the capital of Italy?", "answers": ["Rome", "Paris", "Oslo", "Dublin"], "correct": "Rome", "picture": "images/monkey_1.png" },
+	{"text": "What is the capital of Germany?", "answers": ["Zagreb", "London", "Viena", "Berlin"], "correct": "Berlin" },
+	{"text": "What is the capital of Ireland?", "answers": ["Belfast", "Dublin", "Chaco", "Madrid"], "correct": "Dublin" },
   ];
   
 	var score = 0
-	var slide = ["images/monkey_1.png", "images/monkey_2.png"]
-
+	var slide = ["images/monkey_1.png", "images/monkey_2.png", "images/monkey_3.png"]
+	var sound = ["monkeysound.wav"]
 	
-	  
+
+	 
 	var display_question = (function () {
 		var question = questions[score];
 		
@@ -27,24 +26,20 @@ $(document).on("ready", function(){
 	});
 	
 
-	
-	
-	
-	
 	var validate_answer = (function () {
 	
 		if ($(this).text() == questions[score].correct) {
 			score++;
 			
-		
-			
-			 
+		$('#slides_' + score).slideDown(3000);
+		$('body').append('<embed src="sounds/monkeysound.wav" autostart="true" width="1" height="1" id="LegacySound" enablejavascript="true">');
+            $('#ticks').fadeIn(4000);
 			
 			if (questions.length > score) {
 				display_question();
 				countdown = max_countdown;
 			} else {
-				$("#question").text("you are great quizz finished!");
+				$("#question").text("you are great quiz finished!");
 				$("#answer").empty();
 				clearInterval(countdown_interval);
 			}	
@@ -66,7 +61,7 @@ $(document).on("ready", function(){
 			
 	});
 	
-	var max_countdown = 1000000000000;
+	var max_countdown = 100;
 	var countdown = max_countdown;
 	var display_countdown = (function() {
 		$("#countdown").text(countdown);
