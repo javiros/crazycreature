f03e50489da23ef29478bf9b7981ce86bf8c6570
paul mannion <mannion.paul@gmail.com>
2013-05-03 18:38:12 +0100

    sounds removed from satge 1 & 2

--- script_stage2.js
+++ script_stage2.js
@@ -46,7 +46,7 @@ $('body').append('<embed src="sounds/drums.mp3" autostart="true" width="1" heigh
 				countdown = max_countdown;
 			} else {
 				$("#question").text("Wow an Octopus! You're doing great!");
-				$('#next_button').effect("shake", { times:2 }, 2000).append('<embed src="sounds/kidcheer.wav" autostart="true" width="1" height="1" id="LegacySound" enablejavascript="true">');
+				$('#next_button').effect("shake", { times:2 }, 2000);
 				$('#slides').effect("bounce", { times:3 }, 2000);
 				$("#answer").empty();
 				clearInterval(countdown_interval);
